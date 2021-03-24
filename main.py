1ba174ad86980bfccef1c0f6e7e5e0180cc94814
Uhanalainen <loukaja@gmail.com>
2021-03-24 19:24:24 +0200

    Add ball widget

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
+from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.vector import Vector
 
 
 class PongGame(Widget):
@@ -11,6 +13,17 @@ class PongApp(App):
 
     def build(self):
         return PongGame()
+
+
+class PongBall(Widget):
+
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 if __name__ == '__main__':
